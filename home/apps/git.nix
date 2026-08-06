8a74f1a7a9313e8bde78656e70d3e3849e856ae5
somnvl <186089420+somnvl@users.noreply.github.com>
2026-08-07 01:43:51 +0200

    V0.3 hyprland - fix profile fit

--- home/apps/git.nix
+++ home/apps/git.nix
@@ -3,8 +3,8 @@
   programs.git = {
     enable = true;
     settings = {
-      user.name = profile.gitName;
-      user.email = profile.gitEmail;
+      user.name = profile.git.name;
+      user.email = profile.git.email;
       alias.fast = "!f() { git add . && git commit -m \"$@\" && git push && git push backup; }; f";
       alias.origin = "!f() { git add . && git commit -m \"$@\" && git push; }; f";
       alias.backup = "!f() { git add . && git commit -m \"$@\" && git push backup; }; f";
