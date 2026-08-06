2b51da0761822e4775fdc8266efd9ad683a7e36c
so <186089420+somnvl@users.noreply.github.com>
2026-08-06 21:01:44 +0200

    V0 skeleton - Add Zsh shell aliases configuration

---
+++ home/apps/aliases.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  programs.zsh.shellAliases = {
+    # WORK
+    ccc = "cc -Wall -Wextra -Werror";
+    vg = "valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes";
+    p3 = "python3";
+    cx = "chmod +x";
+
+    # SHELL
+    ll  = "ls -lah --color=auto";
+    gcl = "git clone";
+
+    wifi = "sudo nmtui";
+    bluetooth = "sudo bluetui";
+  };
+}
