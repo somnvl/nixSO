b82316b508539cc21c45f0578ff198c9bd44d447
so <186089420+somnvl@users.noreply.github.com>
2026-08-06 21:00:31 +0200

    Create zsh.nix

---
+++ home/apps/zsh.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+{
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    dotDir = "${config.xdg.configHome}/zsh";
+
+    envExtra = ''
+      export POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
+    '';
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "sudo" ];
+    };
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+    initContent = ''
+      export XDG_RUNTIME_DIR="''${XDG_RUNTIME_DIR:-/run/user/''$UID}"
+      export TMPDIR="''${XDG_RUNTIME_DIR}"
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+    '';
+  };
+
+  home.file.".p10k.zsh".source = ../../dotfiles/config/p10k/.p10k.zsh;
+}
