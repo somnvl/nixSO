cd5c6d0f93969aa1c792423e1140dbaa74f8fcfb
somnvl <186089420+somnvl@users.noreply.github.com>
2026-08-07 04:04:03 +0200

    V0.3.1 hyprland scroll overview - binds

--- dotfiles/config/hypr/input.lua
+++ dotfiles/config/hypr/input.lua
@@ -22,11 +22,11 @@ hl.config({
     },
 
     gestures = {
-        workspace_swipe_distance = 300,
+        workspace_swipe_distance = 400,
         workspace_swipe_invert = true,
         workspace_swipe_min_speed_to_force = 30,
         workspace_swipe_direction_lock = true,
         workspace_swipe_direction_lock_threshold = 10,
-        workspace_swipe_create_new = false,
+        workspace_swipe_create_new = true,
     },
 })
--- dotfiles/config/hypr/scrolloverview.lua
+++ dotfiles/config/hypr/scrolloverview.lua
@@ -4,13 +4,18 @@ if hl.plugin and hl.plugin.scrolloverview then
     hl.plugin.scrolloverview.configure({
         scale = 0.5,
         layout = "vertical",
-        workspace_gap = 100,
+        workspace_gap = 50,
         wallpaper = 0,
         blur = true,
 
         shadow = {
             enabled = false,
         },
+    })
+
+    hl.plugin.scrolloverview.gesture({
+        fingers = 4,
+        direction = "vertical",
     })
 
     hl.bind(mainMod .. " + Tab", function()
