ee69d9d21d040c01803777f1825b0a0a42a1cbb8
somnvl <186089420+somnvl@users.noreply.github.com>
2026-08-09 02:10:35 +0200

    V0.4.3 - hyprland gestures tweaks

--- dotfiles/config/hypr/gestures.lua
+++ dotfiles/config/hypr/gestures.lua
@@ -14,7 +14,7 @@ hl.config({
     gestures = {
         workspace_swipe_distance = 300,
         workspace_swipe_invert = true,
-        workspace_swipe_min_speed_to_force = 20,
+        workspace_swipe_min_speed_to_force = 0,
         workspace_swipe_direction_lock = true,
         workspace_swipe_direction_lock_threshold = 10,
         workspace_swipe_create_new = true,
