3562b3451a55fe2c28061c3af926618fe3852841
somnvl <186089420+somnvl@users.noreply.github.com>
2026-08-11 00:05:16 +0200

    V0.5.0 - themed ok

--- home/apps/quickshell.nix
+++ home/apps/quickshell.nix
@@ -2,6 +2,10 @@
 {
   home.packages = [ pkgs.quickshell ];
 
+  home.sessionVariables = {
+    QS_NO_RELOAD_POPUP = "1";
+  };
+
   xdg.configFile."quickshell" = {
     source = ../../dotfiles/config/quickshell;
     recursive = true;
